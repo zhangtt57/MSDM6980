62e069155c26aa468d98f647e0d62fbe768d1ebc
zhangtt57 <zhangtt57@163.com>
2025-02-12 15:38:35 +0800

    Update ionosphere.py

--- ionosphere/ionosphere.py
+++ ionosphere/ionosphere.py
@@ -142,8 +142,8 @@ def QUBO_FS_classif_RF_1_vs_all(features, k, labels):
 import matplotlib.pyplot as plt
 
 results_all_features = classif_RF_1_vs_all(features, labels)
-results_random_fs = random_FS_classif_RF_1_vs_all(features, k=5, labels=labels)
-results_qubo_fs = QUBO_FS_classif_RF_1_vs_all(features, k=5, labels=labels)
+results_random_fs = random_FS_classif_RF_1_vs_all(features, k=5, labels)
+results_qubo_fs = QUBO_FS_classif_RF_1_vs_all(features, k=5, labels)
 
 comparison_data = {}
 
